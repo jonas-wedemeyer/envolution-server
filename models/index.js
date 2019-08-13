4ff5b5c5392ca97f1ad0062640ee3fbcb61214ba
miquintero <schaeffer.quintero@gmail.com>
2019-08-13 18:10:13 +0200

    feat: initialized sequelize

---
+++ models/index.js
@@ -0,0 +1,49 @@
+const env = process.env.NODE_ENV || 'development';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const config = require('./config')[env];
+
+const basename = path.basename(__filename);
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: config.dialect,
+    logging: false,
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    operatorsAliases: false,
+  },
+);
+
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
+  })
+  .forEach(file => {
+    var model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
