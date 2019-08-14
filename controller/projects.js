2edcd2a9d007c6efc640e2e4eb9308f230f86a2c
miquintero <schaeffer.quintero@gmail.com>
2019-08-14 18:35:42 +0200

    feat: drafted project controllers

--- controller/projects.js
+++ controller/projects.js
@@ -1,5 +1,42 @@
-exports.getProjectList = async (ctx, next) => {};
+const { project: Project } = require('../models/projects');
 
-exports.getOneProject = ctx => {};
+exports.getProjectList = async (ctx, next) => {
+  try {
+    const projectList = await Project.findAll();
+    const filteredProjects = projectList.filter(
+      project => project.city === ctx.params.city,
+    );
+    ctx.body = {
+      status: 'success',
+      data: filteredProjects,
+    };
+  } catch (error) {
+    ctx.status = error.status || 500;
+    ctx.body = error.message;
+  }
+};
 
-exports.updatePax = ctx => {};
+exports.getOneProject = async (ctx, next) => {
+  try {
+    const project = await Project.findOne(ctx.params.id);
+    ctx.body = {
+      status: 'success',
+      data: project,
+    };
+  } catch (error) {
+    ctx.status = error.status || 500;
+    ctx.body = error.message;
+  }
+};
+
+exports.updatePax = async (ctx, next) => {
+  try {
+    Project.update(
+      { userId: ctx.request.body },
+      { returning: true, where: { projectId: ctx.params.projectId } },
+    );
+  } catch (error) {
+    ctx.status = error.status || 500;
+    ctx.body = error.message;
+  }
+};
