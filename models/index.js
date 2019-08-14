2044a13d7a5ccefa41b570fe7f9b2694098a9792
miquintero <schaeffer.quintero@gmail.com>
2019-08-14 11:54:55 +0200

    fix: altered conditions for file filtering

--- models/index.js
+++ models/index.js
@@ -3,9 +3,8 @@ const env = process.env.NODE_ENV || 'development';
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const config = require('./config')[env];
 
-const basename = path.basename(__filename);
+const config = require('./config')[env];
 const db = {};
 
 const sequelize = new Sequelize(
@@ -14,8 +13,7 @@ const sequelize = new Sequelize(
   config.password,
   {
     host: config.host,
-    dialect: config.dialect,
-    logging: false,
+    dialect: 'postgres',
     pool: {
       max: 10,
       min: 0,
@@ -29,11 +27,14 @@ const sequelize = new Sequelize(
 fs.readdirSync(__dirname)
   .filter(file => {
     return (
-      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+      file.indexOf('.') !== 0 &&
+      file !== 'index.js' &&
+      file !== 'config.js' &&
+      file.slice(-3) === '.js'
     );
   })
   .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
