8b113663ac57224326b5a084fdcca4733cf0e622
miquintero <schaeffer.quintero@gmail.com>
2019-08-15 11:06:23 +0200

    feat: added endpoint for post request

--- router/index.js
+++ router/index.js
@@ -5,11 +5,13 @@ const {
   getProjectList,
   getOneProject,
   updatePax,
+  createProject,
 } = require('../controller/projects');
 const { getOneOrg } = require('../controller/organizations');
 
 router.get('/projects', getProjectList);
 router.get('/projects/:id', getOneProject);
+router.post('/projects', createProject);
 router.put('/projects/:id', updatePax);
 
 router.get('/orgs/:id', getOneOrg);
