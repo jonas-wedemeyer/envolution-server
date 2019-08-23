344022bbf84ccfdd16862b9dc6723057ca424fd3
miquintero <schaeffer.quintero@gmail.com>
2019-08-23 19:58:33 +0200

    fix: modified seeding script

--- scripts/seed-db.sh
+++ scripts/seed-db.sh
@@ -4,4 +4,4 @@ set -a
 . .env
 set +a
 
-DB_HOST=localhost npx sequelize db:seed:all --config ./db/config.js --seeders-path ./db/seed-data
+DB_HOST=localhost npx sequelize db:seed:all --config ./db/config.js --seeders-path ./db/seeders
