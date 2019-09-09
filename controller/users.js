c944b107f3552a14da9334a907a52d49b5fc43e6
Jonas Wedemeyer <jo.wedemeyer@gmail.com>
2019-09-09 16:15:48 +0200

    refactor: removed uneccessary components for image uploading

--- controller/users.js
+++ controller/users.js
@@ -1,8 +1,6 @@
 const bcrypt = require('bcrypt');
 const atob = require('atob');
 const jwt = require('jsonwebtoken');
-const cloudinary = require('cloudinary').v2;
-const path = require('path');
 
 const { user: User, project: Project } = require('../db/models');
 const { filterProps } = require('../utils/filterProps');
@@ -145,27 +143,6 @@ exports.editUser = async (ctx, next) => {
     } else {
       ctx.status = 404;
     }
-  } catch (error) {
-    ctx.status = error.status || 500;
-    ctx.body = error.message;
-  }
-};
-
-exports.uploadPicture = async (ctx, next) => {
-  try {
-    const picture = await cloudinary.uploader.upload(
-      path.resolve(ctx.request.body.files),
-      { format: 'jpg' },
-    );
-    const { email } = JSON.parse(
-      atob(ctx.request.header.authorization.split('.')[1]),
-    );
-    const user = await User.findOne({
-      where: { email },
-    });
-    await user.update({ picture: picture.secure_url });
-    ctx.status = 200;
-    ctx.body = { picture: picture.secure_url };
   } catch (error) {
     ctx.status = error.status || 500;
     ctx.body = error.message;
